928de51fdfc8e2ce2301e6c62fd58662675d1a4f
Chris F Ravenscroft <chris@voilaweb.com>
2022-06-26 15:30:25 -0700

    Slideshow, no cursor...

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.18
 require (
 	github.com/alexflint/go-arg v1.4.3
 	github.com/disintegration/imaging v1.6.2
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 )
 
 require (
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -13,7 +13,11 @@ import (
 	"image/png"
 	"strings"
 	"syscall"
+	"time"
 	"unsafe"
+
+	"github.com/eiannone/keyboard"
+	"golang.org/x/sys/unix"
 
 	arg "github.com/alexflint/go-arg"
 	"github.com/disintegration/imaging"
@@ -64,33 +68,33 @@ type fb_var_screeninfo struct {
 const FBIOGET_FSCREENINFO = 0x4602
 const FBIOGET_VSCREENINFO = 0x4600
 
-var args struct {
-	ImgPath    string   `arg:"positional,required"`
+type args struct {
+	ImgPath    []string `arg:"positional,required"`
 	DevicePath string   `default:"/dev/fb0"`
-	Transform  []string `arg:"separate" help:"fit|center"`
-	DontClear  bool
+	Transform  []string `arg:"separate" help:"can be invoked multiple times\n                         accepted: fit hfit vfit center"`
+	DontClear  bool     `help:"do not clear screen before rendering image"`
+	NoCursor   bool     `help:"hide console cursor"`
+	Redraw     int      `help:"keep re-rendering image every n seconds, hiding console output"`
 	Verbose    bool
 }
 
-func main() {
-	arg.MustParse(&args)
-	imgF, err := os.Open(args.ImgPath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer imgF.Close()
+type imgContext struct {
+	image          image.Image
+	image_width    int
+	image_height   int
+	image_xoffset  int
+	image_yoffset  int
+	screen_xoffset int
+	screen_yoffset int
+}
 
-	var img image.Image
-	if strings.HasSuffix(args.ImgPath, ".png") {
-		img, err = png.Decode(imgF)
-	} else if strings.HasSuffix(args.ImgPath, ".jpg") || strings.HasSuffix(args.ImgPath, ".jpeg") {
-		img, err = jpeg.Decode(imgF)
-	}
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+func (args) Description() string {
+	return "Display an image in your graphical console using the frame buffer.\nYou may apply multiple transformations.\n"
+}
+
+func main() {
+	var args args
+	arg.MustParse(&args)
 
 	fbF, err := os.OpenFile(args.DevicePath, os.O_RDWR, os.ModeDevice)
 	if err != nil {
@@ -98,6 +102,20 @@ func main() {
 		return
 	}
 	defer fbF.Close()
+
+	if args.NoCursor {
+		fbT, err := os.OpenFile("/dev/console", unix.O_WRONLY, 0)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer func() {
+			fbT.WriteString("\033[?25h")
+			time.Sleep(1 * time.Second)
+			fbT.Close()
+		}()
+		fbT.WriteString("\033[?25l")
+	}
 
 	screeninfo := fb_var_screeninfo{}
 	uscreeninfo := unsafe.Pointer(&screeninfo)
@@ -113,47 +131,98 @@ func main() {
 		fmt.Println("Screen information:", screen_width, screen_height, bpp)
 	}
 
-	image_xoffset := 0
-	image_yoffset := 0
-	screen_xoffset := 0
-	screen_yoffset := 0
+	imageContexts := []imgContext{}
 
-	var wImg image.Image
-	wImg = img
-	for _, transform := range args.Transform {
-		image_xoffset, image_yoffset, screen_xoffset, screen_yoffset = 0, 0, 0, 0
-		if transform == "fit" {
-			if args.Verbose {
-				fmt.Println("Image size before resizing:", wImg.Bounds())
-			}
-			wImg = imaging.Resize(wImg, screen_width, screen_height, imaging.Lanczos)
-			if args.Verbose {
-				fmt.Println("Image size after resizing:", wImg.Bounds())
-			}
-		} else if transform == "center" {
-			imgWidth := wImg.Bounds().Max.X
-			imgHeight := wImg.Bounds().Max.Y
-			if imgWidth > screen_width {
-				image_xoffset = (imgWidth - screen_width) / 2
-			} else if imgWidth < screen_width {
-				screen_xoffset = (screen_width - imgWidth) / 2
-			}
-			if imgHeight > screen_height {
-				image_yoffset = (imgHeight - screen_height) / 2
-			} else if imgHeight < screen_height {
-				screen_yoffset = (screen_height - imgHeight) / 2
-			}
-			if args.Verbose {
-				fmt.Println("Image size:", wImg.Bounds())
+	for _, imgPath := range args.ImgPath {
+		imageContext := imgContext{}
+
+		imgF, err := os.Open(imgPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer imgF.Close()
+
+		var img image.Image
+		if strings.HasSuffix(imgPath, ".png") {
+			img, err = png.Decode(imgF)
+		} else if strings.HasSuffix(imgPath, ".jpg") || strings.HasSuffix(imgPath, ".jpeg") {
+			img, err = jpeg.Decode(imgF)
+		}
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var wImg image.Image
+		wImg = img
+		for _, transform := range args.Transform {
+			imageContext.image_xoffset, imageContext.image_yoffset, imageContext.screen_xoffset, imageContext.screen_yoffset = 0, 0, 0, 0
+			if transform == "fit" {
+				if args.Verbose {
+					fmt.Println("Image size before resizing:", wImg.Bounds())
+				}
+				wImg = imaging.Resize(wImg, screen_width, screen_height, imaging.Lanczos)
+				if args.Verbose {
+					fmt.Println("Image size after resizing:", wImg.Bounds())
+				}
+			} else if transform == "hfit" {
+				if args.Verbose {
+					fmt.Println("Image size before horizontal resizing:", wImg.Bounds())
+				}
+				wImg = imaging.Resize(wImg, screen_width, wImg.Bounds().Dy(), imaging.Lanczos)
+				if args.Verbose {
+					fmt.Println("Image size after resizing:", wImg.Bounds())
+				}
+			} else if transform == "vfit" {
+				if args.Verbose {
+					fmt.Println("Image size before vertical resizing:", wImg.Bounds())
+				}
+				wImg = imaging.Resize(wImg, wImg.Bounds().Dx(), screen_height, imaging.Lanczos)
+				if args.Verbose {
+					fmt.Println("Image size after resizing:", wImg.Bounds())
+				}
+			} else if transform == "center" {
+				imgWidth := wImg.Bounds().Max.X
+				imgHeight := wImg.Bounds().Max.Y
+				if imgWidth > screen_width {
+					imageContext.image_xoffset = (imgWidth - screen_width) / 2
+				} else if imgWidth < screen_width {
+					imageContext.screen_xoffset = (screen_width - imgWidth) / 2
+				}
+				if imgHeight > screen_height {
+					imageContext.image_yoffset = (imgHeight - screen_height) / 2
+				} else if imgHeight < screen_height {
+					imageContext.screen_yoffset = (screen_height - imgHeight) / 2
+				}
+				if args.Verbose {
+					fmt.Println("Image size:", wImg.Bounds())
+				}
 			}
 		}
-	}
 
-	_, ok := wImg.At(0, 0).(color.NRGBA)
-	if !ok {
-		convertedImg := image.NewNRGBA(image.Rect(0, 0, wImg.Bounds().Dx(), wImg.Bounds().Dy()))
-		draw.Draw(convertedImg, convertedImg.Bounds(), wImg, wImg.Bounds().Min, draw.Src)
-		wImg = convertedImg
+		_, ok := wImg.At(0, 0).(color.NRGBA)
+		if !ok {
+			convertedImg := image.NewNRGBA(image.Rect(0, 0, wImg.Bounds().Dx(), wImg.Bounds().Dy()))
+			draw.Draw(convertedImg, convertedImg.Bounds(), wImg, wImg.Bounds().Min, draw.Src)
+			wImg = convertedImg
+		}
+
+		imageContext.image = wImg
+		imageContext.image_width = wImg.Bounds().Max.X
+		if imageContext.image_width > screen_width {
+			imageContext.image_width = screen_width
+		}
+		imageContext.image_height = wImg.Bounds().Max.Y
+		if imageContext.image_height > screen_height {
+			imageContext.image_height = screen_height
+		}
+		if args.Verbose {
+			fmt.Println("y from", imageContext.image_yoffset, "to", imageContext.image_yoffset+imageContext.image_height, "x from", imageContext.image_xoffset, "to", imageContext.image_xoffset+imageContext.image_width)
+			fmt.Println("screen y from", imageContext.screen_yoffset, "screen x from", imageContext.screen_xoffset)
+		}
+
+		imageContexts = append(imageContexts, imageContext)
 	}
 
 	screenPixels, err := syscall.Mmap(
@@ -168,42 +237,65 @@ func main() {
 	}
 	defer syscall.Munmap(screenPixels)
 
-	imgWidth := wImg.Bounds().Max.X
-	if imgWidth > screen_width {
-		imgWidth = screen_width
+	keysEvents, err := keyboard.GetKeys(1)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	imgHeight := wImg.Bounds().Max.Y
-	if imgHeight > screen_height {
-		imgHeight = screen_height
-	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
 
-	if !args.DontClear {
-		for i := 0; i < screen_height*screen_width*4; i++ {
-			screenPixels[i] = 0
+	curImageContextIdx := 0
+	for {
+		if !args.DontClear {
+			for i := 0; i < screen_height*screen_width*4; i++ {
+				screenPixels[i] = 0
+			}
 		}
-	}
 
-	curPixelBit := (screen_yoffset*screen_width + screen_xoffset) * 4
-	if args.Verbose {
-		fmt.Println("y from", image_yoffset, "to", image_yoffset+imgHeight, "x from", image_xoffset, "to", image_xoffset+imgWidth)
-		fmt.Println("screen y from", screen_yoffset, "screen x from", screen_xoffset)
-	}
+		if args.Verbose {
+			fmt.Println("Reading image:", curImageContextIdx)
+		}
+		imageContext := imageContexts[curImageContextIdx]
 
-	for y := image_yoffset; y < image_yoffset+imgHeight; y++ {
-		for x := image_xoffset; x < image_xoffset+imgWidth; x++ {
-			pixColor := wImg.At(x, y)
-			pixColorBits := pixColor.(color.NRGBA)
-			screenPixels[curPixelBit] = pixColorBits.R
-			curPixelBit++
-			screenPixels[curPixelBit] = pixColorBits.G
-			curPixelBit++
-			screenPixels[curPixelBit] = pixColorBits.B
-			curPixelBit++
-			screenPixels[curPixelBit] = pixColorBits.A
-			curPixelBit++
+		curPixelBit := (imageContext.screen_yoffset*screen_width + imageContext.screen_xoffset) * 4
+		for y := imageContext.image_yoffset; y < imageContext.image_yoffset+imageContext.image_height; y++ {
+			for x := imageContext.image_xoffset; x < imageContext.image_xoffset+imageContext.image_width; x++ {
+				pixColor := imageContext.image.At(x, y)
+				pixColorBits := pixColor.(color.NRGBA)
+				screenPixels[curPixelBit] = pixColorBits.R
+				curPixelBit++
+				screenPixels[curPixelBit] = pixColorBits.G
+				curPixelBit++
+				screenPixels[curPixelBit] = pixColorBits.B
+				curPixelBit++
+				screenPixels[curPixelBit] = pixColorBits.A
+				curPixelBit++
+			}
+			if screen_width > imageContext.image_width {
+				curPixelBit += (screen_width - imageContext.image_width) * 4
+			}
 		}
-		if screen_width > imgWidth {
-			curPixelBit += (screen_width - imgWidth) * 4
+
+		if len(imageContexts) == curImageContextIdx {
+			if args.Redraw == 0 {
+				break
+			}
 		}
+		curImageContextIdx++
+		if curImageContextIdx >= len(imageContexts) {
+			curImageContextIdx = 0
+		}
+
+		select {
+		case event := <-keysEvents:
+			if event.Key == keyboard.KeyEsc {
+				return
+			}
+		default:
+		}
+
+		time.Sleep(time.Duration(args.Redraw) * time.Second)
 	}
 }
